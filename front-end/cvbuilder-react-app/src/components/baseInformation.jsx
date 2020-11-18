ce7e791474b66770e01b2920645c5153e1c4c35c
anfnamazi <anf.namazi@gmail.com>
2020-11-18 19:33:20 +0330

    fixed problem time stamp in date picker

--- front-end/cvbuilder-react-app/src/components/baseInformation.jsx
+++ front-end/cvbuilder-react-app/src/components/baseInformation.jsx
@@ -105,7 +105,7 @@ const BaseInfo = () => {
     const military = event.target.military.value;
     const description = event.target.description.value;
     const image = event.target.image.files[0];
-    const birthDay = birth._d.toLocaleDateString("en-CA");
+    const birthDay = new Date(birth).toLocaleDateString("en-CA");
 
     const email = event.target.email.value;
     const phone = event.target.phone.value;
