744f2657d49b84eda0f302302a71b1a2e707e18e
mamad <chorom.mamad@gmail.com>
2020-11-20 01:12:21 +0330

    remove admin in getusers

--- back-end/app/controllers/users/getUsers.js
+++ back-end/app/controllers/users/getUsers.js
@@ -1,4 +1,5 @@
 const User = require('../../models/user')
+const _ = require('lodash')
 const { handleError } = require('../../middleware/utils')
 const { getItems, checkQueryString } = require('../../middleware/db')
 
@@ -10,7 +11,12 @@ const { getItems, checkQueryString } = require('../../middleware/db')
 const getUsers = async (req, res) => {
   try {
     const query = await checkQueryString(req.query)
-    res.status(200).json(await getItems(req, User, query))
+    let usersResult = await getItems(req, User, query)
+    _.remove(usersResult.docs, {
+      role: 'admin'
+    })
+    console.log(usersResult)
+    res.status(200).json(usersResult)
   } catch (error) {
     handleError(res, error)
   }
