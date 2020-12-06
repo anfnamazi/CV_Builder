ff02ff362963578b208cd1b2c07b7925037c9def
mamad <chorom.mamad@gmail.com>
2020-12-07 00:27:13 +0330

    jobHistory to list

--- back-end/app/controllers/users/createJobHistoryUser.js
+++ back-end/app/controllers/users/createJobHistoryUser.js
@@ -1,6 +1,7 @@
 const { matchedData } = require('express-validator')
 const { handleError } = require('../../middleware/utils')
 const { createJobHistoryInDB } = require('./helpers')
+const model = require('../../models/jobHistory')
 
 /**
  * Create contact item for user function called by route
@@ -10,11 +11,23 @@ const { createJobHistoryInDB } = require('./helpers')
 const createJobHistoryUser = async (req, res) => {
   try {
     let user = req.user
-    req = matchedData(req)
-    const item = await createJobHistoryInDB(req, user._id)
-    user.jobHistories.push(item._id)
-    await user.save()
-    res.status(201).json({ 'new item': item })
+    const edusCount = req.body.length
+    let items = []
+    const resultDelete = await model.deleteMany({ user: user._id })
+    if (resultDelete) {
+      user.jobHistories = []
+      req.body.map(async (jobField) => {
+        req = matchedData(req)
+        const item = await createJobHistoryInDB(jobField, user._id)
+        user.jobHistories.push(item._id)
+
+        items.push(item)
+        if (edusCount === items.length) {
+          await user.save()
+          res.status(201).json({ 'new items': items })
+        }
+      })
+    }
   } catch (error) {
     handleError(res, error)
   }
